cf15a8042acc71ad86c1bf136b30bd5806c13629
sanjidnet <contact@sanjid.net>
2024-05-23 00:36:38 +1200

    enable backend for github action only

--- versions.tf
+++ versions.tf
@@ -6,12 +6,12 @@ terraform {
     }
   }
 
-  backend "s3" {
-    # profile        = "sanjid_mac"
-    bucket         = "sanjidnet-tf-state-ap-southeast-2"
-    key            = "bootstrap/terraform.tfstate"
-    dynamodb_table = "dynamo"
-    region         = "ap-southeast-2"
-    encrypt        = "true"
-  }
+  # backend "s3" {
+  #   # profile        = "sanjid_mac"
+  #   bucket         = "sanjidnet-tf-state-ap-southeast-2"
+  #   key            = "bootstrap/terraform.tfstate"
+  #   dynamodb_table = "dynamo"
+  #   region         = "ap-southeast-2"
+  #   encrypt        = "true"
+  # }
 }
